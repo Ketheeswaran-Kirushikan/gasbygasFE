79793b8d77166056285051a81308ebfb61c3f717
Ketheeswaran-Kirushikan <kirushikanketheeswaran@gmail.com>
2025-02-16 11:52:09 +0530

    final update issue

--- components/outlet/users/user-management.tsx
+++ components/outlet/users/user-management.tsx
@@ -25,7 +25,7 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import { Search, Plus, Eye, ChevronLeft, ChevronRight } from "lucide-react";
+import { Search, Plus, Eye, ChevronLeft, ChevronRight,Trash2 } from "lucide-react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { UserForm } from "./user-form";
